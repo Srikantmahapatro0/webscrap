73d22ac903e3df0c8b1b45bc8e18976072b879bf
Srikantmahapatro0 <srikantmahapatro0@gmail.com>
2024-11-14 22:47:45 +0530

    bug fixed

--- webscrap/src/main/java/com/webscrap/webscrap/Configuration/ScheduleScraping.java
+++ webscrap/src/main/java/com/webscrap/webscrap/Configuration/ScheduleScraping.java
@@ -1,22 +1,34 @@
 package com.webscrap.webscrap.Configuration;
+import com.webscrap.webscrap.Controller.ScrapeController;
+import com.webscrap.webscrap.Models.ScrapeRequest;
 import com.webscrap.webscrap.Service.Scrapeservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+@Service
 public class ScheduleScraping {
-    @Service
+
     public class ScheduledScrapingService {
         @Autowired
-        private Scrapeservice webScrapingService;
+        private ScrapeController sc;
 
-        @Scheduled(fixedRate = 3600000)  // Every hour
-        public void scheduleScraping() {
+        private final List<String> urls = Arrays.asList("https://example.com", "https://anotherexample.com");
+        private final List<String> keywords = Arrays.asList("technology", "innovation");
 
-            // Add logic to retrieve URLs and keywords from storage
-            // Example: webScrapingService.scrapeData(url, keyword);
+        @Scheduled(fixedRate = 36000)
+        public void schedule() {
+            try { sc.scrape(urls.get(0), keywords.get(0)); } catch (IOException e) { e.printStackTrace(); }
+
+
         }
     }
-
-
 }
--- webscrap/src/main/java/com/webscrap/webscrap/Controller/SearchingController.java
+++ webscrap/src/main/java/com/webscrap/webscrap/Controller/SearchingController.java
@@ -20,7 +20,6 @@ import java.util.List;
 
         @GetMapping("/status/{jobId}")
         public ResponseEntity<StatusResponse> getStatus(@PathVariable String jobId) {
-            // Mocked response for example
             StatusResponse response = new StatusResponse("completed", jobId, List.of("url1", "url2"), List.of("keyword1", "keyword2"), "2 MB", "2024-11-01T10:15:00Z");
             return ResponseEntity.ok(response);
         }
